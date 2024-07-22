f817837d5efd91684251b59cfc70b7e7ed81b5ee
sanjuna s prasad <sanjunasprasad@gmail.com>
2024-07-22 21:06:08 +0530

    axios changed

--- src/services/axios/axios.js
+++ src/services/axios/axios.js
@@ -1,30 +1,65 @@
-import axios from 'axios';
-import { apiURl } from '../../utils/constants';
-function createAxiosInstance(token ,role ) { 
-    const instance = axios.create({
-        baseURL: apiURl,
-    });
-    if( token && token.trim()){
-    instance.interceptors.request.use(
+// import axios from 'axios';
+// import { apiURl } from '../../utils/constants';
+// function createAxiosInstance(token  ) { 
+//     const instance = axios.create({
+//         baseURL: apiURl,
+//     });
+//     if( token && token.trim()){
+//     instance.interceptors.request.use(
         
-        (config) => {
-            config.headers.Authorization = `Bearer ${localStorage.getItem(token)}`; 
-            config.headers.role = role;
-            return config;
-        },
-        (error) => {
-            return Promise.reject(error);
-        }
-      );
+//         (config) => {
+//             config.headers.Authorization = `Bearer ${localStorage.getItem(token)}`; 
+//             return config;
+//         },
+//         (error) => {
+//             return Promise.reject(error);
+//         }
+//       );
+//     }
+//     return instance;
+// }
+
+
+
+// const axiosUserInstance = createAxiosInstance("token" );
+// const axiosInstance = createAxiosInstance(null , null)
+
+
+// export {
+//     axiosUserInstance,
+//     axiosInstance
+// };
+
+
+import axios from 'axios';
+import { apiURL } from '../../utils/constants';
+
+function createAxiosInstance(token) {
+    const instance = axios.create({
+        baseURL: apiURL,
+        withCredentials: true, 
+    });
+
+    if (token) {
+        instance.interceptors.request.use(
+            (config) => {
+                const authToken = localStorage.getItem('token'); 
+                if (authToken) {
+                    config.headers.Authorization = `Bearer ${authToken}`;
+                }
+                return config;
+            },
+            (error) => {
+                return Promise.reject(error);
+            }
+        );
     }
+
     return instance;
 }
 
-
-
-const axiosUserInstance = createAxiosInstance("token" );
-const axiosInstance = createAxiosInstance(null , null)
-
+const axiosUserInstance = createAxiosInstance(true); 
+const axiosInstance = createAxiosInstance(false); 
 
 export {
     axiosUserInstance,
